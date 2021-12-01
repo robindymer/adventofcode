45b218722ef4fc608c3d6123e1983dd3f920fb5f
Robin Dymér <robin.dymer@hotmail.com>
2021-12-02 00:49:41 +0100

    Lösning av prob. 1

--- 1_advent.py
+++ 1_advent.py
@@ -1,3 +1,4 @@
+""" DEL 1 """
 depths = []
 prevDepth = 0
 
@@ -7,6 +8,8 @@ numDecreased = 0
 with open("input.txt") as f:
     for line in f:
         depths.append(int(line.strip()))
+
+# with a list of depths, determine how many succeeding depths are bigger than the prev one
 for i in range(len(depths)):
     if i == 0:
         prevDepth = depths[i]
@@ -16,6 +19,31 @@ for i in range(len(depths)):
         numIncreased += 1
     elif depths[i] < prevDepth:
         numDecreased += 1
+    prevDepth = depths[i]
+
+# print(numIncreased)
+# print(numDecreased)
+
+""" DEL 2 """
+prevDepthSum = 0
+numIncreased = 0
+numDecreased = 0
+# sum three consecutive depths, and compare as before
+for i in range(len(depths)):
+    if i == 0:
+        prevDepthSum = depths[i] + depths[i+1] + depths[i+2]
+        continue
+    
+    # stop when we're out of range
+    try:
+        sum = depths[i] + depths[i+1] + depths[i+2]
+        if sum > prevDepthSum:
+            numIncreased += 1
+        elif sum < prevDepthSum:
+            numDecreased += 1
+        prevDepthSum = sum
+    except IndexError:
+        break 
 
 print(numIncreased)
 print(numDecreased)
